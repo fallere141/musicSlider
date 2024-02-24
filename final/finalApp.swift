e38b3fd01b9385e8b07be5cdf749098ce67c9851
zhangzx072@outlook.com <zhangzx@uchicago.edu>
2024-02-24 00:11:31 -0600

    Initial Commit

---
+++ final/finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalApp.swift
+//  final
+//
+//  Created by Fallere141 on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
