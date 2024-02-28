747ccf8f216810f2d4de22c61d7b526376a52d54
veranoa <summer.yun.liu@outlook.com>
2024-02-28 00:45:20 -0600

    deleteView update_2

--- final/View/DetailView.swift
+++ final/View/DetailView.swift
@@ -29,7 +29,7 @@ struct DetailView: View {
         NavigationView {
             GeometryReader { geometry in
                 VStack (alignment: .leading, spacing: 10) {
-                    Spacer().frame(height: 80)
+                    Spacer().frame(height: 70)
                     
                     if songs.indices.contains(currentSongIndex), let artworkURL = songs[currentSongIndex].artwork?.url(width: Int(geometry.size.width * 0.8), height: Int(geometry.size.width * 0.8)) {
                         ZStack(alignment: .center) {
@@ -170,64 +170,74 @@ struct DetailView: View {
                             .foregroundColor(.gray)
                             .padding(.leading)
                             .frame(height: 20)
+                    } else {
+                        Text("")
+                            .frame(height: 70)
+                        Text("")
+                            .frame(height: 20)
                     }
                     
                     Spacer().frame(height: 15)
-                    ScrollView(.horizontal, showsIndicators: false) {
-                        HStack {
-                            ForEach(playlists, id: \.self) { playlist in
-                                VStack(alignment: .center, spacing: 5) {
-                                    AsyncImage(url: playlist.artwork?.url(width: 50, height: 50)) { image in
-                                        image
-                                            .resizable()
-                                            .scaledToFit()
-                                    } placeholder: {
-                                        Image(systemName: "arrow.down")
-                                            .resizable()
-                                            .scaledToFit()
-                                            .frame(width: 40, height: 40)
-                                            .foregroundColor(.gray)
-                                    }
-                                    .frame(width: 60, height: 60)
-                                    .clipShape(Circle())
-                                    .onTapGesture {
-                                        Task{
-                                            await addToPlaylist(song: songs.first!, playlist: playlist)
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text("Save to playlist ...")
+                            .font(.subheadline)
+                            .padding(.horizontal)
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack {
+                                ForEach(playlists, id: \.self) { playlist in
+                                    VStack(alignment: .center, spacing: 5) {
+                                        AsyncImage(url: playlist.artwork?.url(width: 50, height: 50)) { image in
+                                            image
+                                                .resizable()
+                                                .scaledToFit()
+                                        } placeholder: {
+                                            Image(systemName: "arrow.down")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 40, height: 40)
+                                                .foregroundColor(.gray)
                                         }
+                                        .frame(width: 60, height: 60)
+                                        .clipShape(Circle())
+                                        .onTapGesture {
+                                            Task{
+                                                await addToPlaylist(song: songs.first!, playlist: playlist)
+                                            }
+                                        }
+                                        
+                                        Text(playlist.name)
+                                            .font(.caption)
+                                            .lineLimit(2)
+                                            .frame(height: 40)
+                                        
+                                        Spacer()
                                     }
-                                    
-                                    Text(playlist.name)
+                                    .frame(width: 60, height: 100, alignment: .top)
+                                }
+                                VStack(alignment: .center, spacing: 5) {
+                                    Spacer().frame(height: 15)
+                                    Image(systemName: "ellipsis")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 30, height: 30)
+                                        .foregroundColor(.gray)
+                                        .onTapGesture {
+//                                            addNewPlaylist()
+                                        }
+                                    Spacer().frame(height: 5)
+                                    Text("Add playlist")
                                         .font(.caption)
                                         .lineLimit(2)
                                         .frame(height: 40)
-                                        
+                                    
                                     Spacer()
                                 }
                                 .frame(width: 60, height: 100, alignment: .top)
                             }
-                            VStack(alignment: .center, spacing: 5) {
-                                Spacer().frame(height: 15)
-                                Image(systemName: "ellipsis")
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 30, height: 30)
-                                    .foregroundColor(.gray)
-                                    .onTapGesture {
-    //                                    addNewPlaylist()
-                                    }
-                                Spacer().frame(height: 15)
-                                Text("add playlist")
-                                    .font(.caption)
-                                    .lineLimit(2)
-                                    .frame(height: 40)
-                                    
-                                Spacer()
-                            }
-                            .frame(width: 60, height: 100, alignment: .top)
+                            .padding(.horizontal)
                         }
-                        .padding(.horizontal)
+                        .frame(height: 100)
                     }
-                    .frame(height: 100)
                 }
             }
             .navigationBarItems(leading: Button(action: {
@@ -240,7 +250,18 @@ struct DetailView: View {
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     NavigationLink(destination: DeleteView()) {
-                        Image(systemName: "trash")
+                        ZStack(alignment: .topTrailing) {
+                            Image(systemName: "trash")
+                            
+                            if !musicData.shared.deletedSongs.isEmpty {
+                                Text("\(musicData.shared.deletedSongs.count)")
+                                    .font(.caption2)
+                                    .padding(5)
+                                    .foregroundColor(.white)
+                                    .background(Circle().fill(Color.red))
+                                    .offset(x: 8, y: -8)
+                            }
+                        }
                     }
                 }
             }
